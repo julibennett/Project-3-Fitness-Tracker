b3bd7857429ab42778d7b50bfceb44eb65be3d03
bonnil1 <bonnieliu759@yahoo.com>
2024-04-03 20:30:07 -0700

    res component progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,38 @@
 
 import './App.css';
+import { useState, useEffect } from "react"
+import { Route, Routes } from "react-router-dom"
+import Reservation from './components/Reservation'
+import Add from './components/Add'
+
+
 
 function App() {
+
+  //Reservation
+  const resURL = "http://localhost:4000/reservation"
+
+  const [classData, setClassData] = useState(null)
+
+  const fetchClassData = async() => {
+    const response = await fetch(`http://localhost:4000/class/660a062f411f77c08526b7b0`)
+    const data = await response.json()
+    setClassData(data)
+  }
+  
+  useEffect(() => {
+    fetchClassData()
+  }, [])
+
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <Routes>
+        <Route path="/" element={<Reservation URL={resURL} classData={classData}/>}/>
+      </Routes>
+      <Add />
     </div>
   );
 }
--- frontend/src/pages/Reservations.js
+++ frontend/src/pages/Reservations.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import { useParams, useNavigate } from "react-router-dom"
 
-const Reservations = () => {
+const Reservations = (reservation) => {
+  const navigate = useNavigate()
+  const params = useParams()
+  const id = params.id
+
+  const removeReservation = (e) => {
+    e.preventDefault()
+    reservation.deleteReservation(id)
+    navigate("/reservation")
+  }
+
+
   return (
     <div>
       <h1>My Reservations</h1>
-      
+      <strong>{reservation.typeOfClass}</strong>
+      <input type="button" value="Delete" onClick={removeReservation}/>
     </div>
   )
 }
