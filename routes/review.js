b0f1aa4ebbe37c8c89117aab01661f3cf625fb68
julibennett <julibennett4@gmail.com>
2024-04-10 21:22:20 -0700

    debugging res show page

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,8 @@
 const router = require("express").Router()
 const reviewCtrl = require("../controllers/reviewController")
-
+const { verifyToken } = require("../middleware/verifyToken")
 //router.get("/class/:id/review/new", reviewCtrl.new)
-router.post("/class/:id/review", reviewCtrl.create)
-router.put("/class/:id/review/:reviewId/", reviewCtrl.edit)
-router.delete("/class/:id/review/:reviewId", reviewCtrl.destroy)
-
+router.post("/class/:id/review", verifyToken, reviewCtrl.create)
+router.put("/class/:id/review/:reviewId/", verifyToken, reviewCtrl.edit)
+router.delete("/class/:id/review/:reviewId", verifyToken, reviewCtrl.destroy)
 module.exports = router
